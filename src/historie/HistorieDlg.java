52091e4dc7cd4f41c4739465ca896da6e1682f3e
fschuett <fschuett@gymnasium-himmelsthuer.de>
2014-07-01 08:27:53 +0200

    Protokolldatei: Inhalt / Kennung Volltextsuche hinzugefügt

--- src/historie/HistorieDlg.java
+++ src/historie/HistorieDlg.java
@@ -3,6 +3,8 @@ package historie;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
@@ -18,6 +20,10 @@ import javax.swing.JScrollPane;
 
 import schliessfach.SchliessfachApp;
 
+import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
 public class HistorieDlg extends JDialog {
 	private static final boolean DEBUG = false;
 	private JList inhalt;
@@ -32,6 +38,8 @@ public class HistorieDlg extends JDialog {
 	private JRadioButton datum;
 	private JRadioButton kennung;
 	private JComboBox cbRubrik;
+	private JLabel lblEinschrnkung;
+	private JTextField einschraenkung;
 
 	public HistorieDlg(JFrame parent) {
 		super(parent);
@@ -105,6 +113,30 @@ public class HistorieDlg extends JDialog {
 		kennung = new JRadioButton("Kennung");
 		sortierung.add(kennung);
 		auswahlPanel.add(kennung);
+		
+		lblEinschrnkung = new JLabel("Einschränkung:");
+		auswahlPanel.add(lblEinschrnkung);
+		
+		einschraenkung = new JTextField();
+		auswahlPanel.add(einschraenkung);
+		einschraenkung.setColumns(10);
+		einschraenkung.getDocument().addDocumentListener(new DocumentListener(){
+			public void removeUpdate(DocumentEvent de) {
+				if (DEBUG)
+					System.out.println("einschraenkung.removeUpdate: stelle("+de.getOffset()+") anzahl("+de.getLength()+")");
+				aktualisiereInhalt();
+			}
+			public void insertUpdate(DocumentEvent de) {
+				if (DEBUG)
+					System.out.println("einschraenkung.insertUpdate: stelle("+de.getOffset()+") anzahl("+de.getLength()+")");
+				aktualisiereInhalt();
+			}
+			public void changedUpdate(DocumentEvent de) {
+				if (DEBUG)
+					System.out.println("einschraenkung.changedUpdate: stelle("+de.getOffset()+") anzahl("+de.getLength()+")");
+				aktualisiereInhalt();
+			}
+		});
 		kennung.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent ae) {
 				if (DEBUG)
@@ -117,15 +149,34 @@ public class HistorieDlg extends JDialog {
 		setSize(800, 400);
 	}
 
+	private String historieEinschraenkung() {
+		if (einschraenkung == null || "".equals(einschraenkung.getText()))
+			return null;
+		else {
+			return "(h.kennung LIKE '%" + einschraenkung.getText() + "%' OR h.inhalt LIKE '%" + einschraenkung.getText() + "%')";
+		}
+	}
+	
 	private String historieRubrik() {
 		if (rubrik == null || !rubrik.isSelected())
-			return "";
+			return null;
 		else {
-			return "WHERE h.rubrik = historie.Rubrik."
+			return "h.rubrik = historie.Rubrik."
 					+ cbRubrik.getSelectedItem();
 		}
 	}
 
+	private String historieWhere() {
+		String w1 = historieRubrik();
+		String w2 = historieEinschraenkung();
+		if(w1 == null && w2 == null)
+			return "";
+		else if(w1 != null && w2 != null)
+			return "WHERE "+w1+" AND "+w2;
+		else
+			return "WHERE "+(w1 == null? "":w1)+(w2==null?"":w2);
+	}
+	
 	private String historieSortierung() {
 		if (datum == null || kennung == null || datum.isSelected())
 			return "ORDER BY h.zeitpunkt DESC";
@@ -136,7 +187,7 @@ public class HistorieDlg extends JDialog {
 	private void aktualisiereInhalt() {
 		if (em == null || inhalt == null)
 			return;
-		String s = historieQueryString + " " + historieRubrik() + " "
+		String s = historieQueryString + " " + historieWhere() + " "
 				+ historieSortierung();
 		Query q = em.createQuery(s);
 		historieListe = q.getResultList().toArray();
